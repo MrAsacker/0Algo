98a0ad716f47c86fece80dd280cc726398da1af7
MrAsacker <jr.asacker@gmail.com>
2025-12-29 20:45:38 +0530

    fix: update next.js to 15.4.1 to resolve CVE-2025-66478

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,37 +1,3 @@
-// "use client";
-
-// import { Button } from "@/components/ui/button";
-// import { UserButton, SignedIn } from "@clerk/nextjs";
-// import Link from "next/link";
-// import { Github } from "lucide-react";
-
-// const Navbar = () => {
-//   const handleStarProject = () => {
-//     window.open("https://github.com/MrAsacker", "_blank");
-//   };
-
-//   return (
-//     <div className="border-b sticky top-0 z-50 bg-background">
-//       <div className="flex h-16 items-center px-4 container mx-auto">
-//         <div className="font-bold text-2xl flex-1">
-//           <Link href="/" className="flex items-center">
-//             <span ><img src="white-icon.svg" alt="0algo"  className="w-10 h-10 object-contain ml-12"/></span>
-//           </Link>
-//         </div>
-
-//         <div className="flex items-center space-x-4 mr-5">
-         
-//           <SignedIn>
-//             <UserButton />
-//           </SignedIn>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 "use client";
 
 import { Button } from "@/components/ui/button";
